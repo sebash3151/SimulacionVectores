9cd4cc02f3376321a732ba399c3411e1a7c277d3
sebash3151 <sebash31510@gmail.com>
2021-10-13 15:09:44 -0500

    Angulos Y LookAt
    Observar, movimiento al mouse y aceleracion con orbitacion alrededor del mouse

--- Simulacion Vectores/Assets/Scripts/Look/MyCustomLookAt.cs
+++ Simulacion Vectores/Assets/Scripts/Look/MyCustomLookAt.cs
@@ -5,51 +5,78 @@ using UnityEngine;
 public class MyCustomLookAt : MonoBehaviour
 {
     [SerializeField] Color color;
-    VectorPoderoso posicion;
+    Vector2 thisPosicion;
     [SerializeField] float speed = 5f;
-    VectorPoderoso observar;
+    Vector2 observar;
+    [SerializeField] bool observacion = false;
+    [SerializeField] bool moverseSinAcelerar = false;
+    Vector2 velocity = new Vector2(0, 0);
 
-    void Start()
-    {
-        posicion = new VectorPoderoso(transform.position.x, transform.position.y);
-    }
+    [Header("Acelerar")]
+    [SerializeField] bool moverseConAcelerar = false;
+    [SerializeField] Vector2 aceleracion;
+    Vector2 mousePosition;
 
     void Update()
     {
-        var mousePosition = GetWorldMousePosition();
-        RotateZ(LookAtOMG(mousePosition));
+        thisPosicion = transform.position;
+        mousePosition = GetWorldMousePosition();
+        ObservarA();
         Impulso();
+        ImpulsoAcelerado();
     }
 
     private void Impulso()
     {
+        if (moverseSinAcelerar)
+        {
+            observar = mousePosition - thisPosicion;
+            observar.Normalize();
+            Vector2 velocity = observar * speed;
+            Vector3 finalPosition = new Vector3(velocity.x, velocity.y, 0);
+            RotateZ(LookAtOMG(velocity)); 
+            transform.position += finalPosition * Time.deltaTime;
+        }
+    }
+    private void ImpulsoAcelerado()
+    {
+        if (moverseConAcelerar)
+        {
+            aceleracion = mousePosition - thisPosicion;
+            velocity += (aceleracion * Time.deltaTime);
+            Vector3 finalPosition = new Vector3(velocity.x, velocity.y, 0);
 
-        VectorPoderoso velocity = observar.Multiplicar(speed);
-        Vector3 finalPosition = new Vector3(velocity.X, velocity.Y, 0);
-        transform.position += finalPosition * Time.deltaTime;
+            RotateZ(LookAtOMG(velocity));
+            transform.position += (finalPosition * Time.deltaTime);
+        }
     }
 
-    private float LookAtOMG(VectorPoderoso mirar)
+    private float LookAtOMG(Vector2 target)
     {
-        observar = mirar.Resta(posicion);
-        observar.Normalizar();
-        mirar.DibujarVector(color);
-        float direccion = Mathf.Atan2(mirar.Y, mirar.X);
-        RotateZ(direccion);
+        float direccion = Mathf.Atan2(target.y, target.x);
         return (direccion);
     }
 
-    private VectorPoderoso GetWorldMousePosition()
+    private Vector2 GetWorldMousePosition()
     {
         Camera camera = Camera.main;
         Vector3 screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane);
         Vector4 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
-        VectorPoderoso posicionFinal = new VectorPoderoso(worldPos.x, worldPos.y);
+        Vector2 posicionFinal = new Vector2(worldPos.x, worldPos.y);
         return posicionFinal;
     }
 
     private void RotateZ(float radians)
     {
         transform.rotation = Quaternion.Euler(0.0f, 0.0f, radians * Mathf.Rad2Deg);
+    }
+
+    private void ObservarA()
+    {
+        if (observacion)
+        {
+            RotateZ(LookAtOMG(mousePosition - thisPosicion));
+        }
+
     }
 }
